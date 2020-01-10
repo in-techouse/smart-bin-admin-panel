113f4117f4c0509eb3dca3953eada4f59de960d1
Tahira Baig <tahirabaig786@hotmail.com>
2020-01-10 13:31:12 +0500

    working on keys

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASURMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
 /* GET home page. */
 router.get('/', function(req, res) {

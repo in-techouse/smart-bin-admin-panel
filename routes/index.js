fc455990251d9aab6ed484a79b6dc0a738e8c601
Tahira Baig <tahirabaig786@hotmail.com>
2020-01-10 12:49:41 +0500

    working on login screen

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/login', {error: ""});
+});
+
+router.post('/login', function(req, res) {
+  res.json("1")
 });
 
 module.exports = router;

8d1fa0fcfc60b6cfe248460bef5b3c64ebc3b092
Tahira Baig <tahirabaig786@hotmail.com>
2020-01-17 13:01:35 +0500

    working on side bar

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
+
 router.get('/',function(req,res){
-res.json("Hello Admin")
+    res.render("pages/index")
 });
+
+router.get('/addDriver',function(req,res){
+    res.render("pages/addDriver")
+});
+
+router.get('/addTruck',function(req,res){
+    res.render("pages/addTruck")
+});
+
+router.get('/allDrivers',function(req,res){
+    res.render("pages/allDrivers")
+});
+
+router.get('/allTrucks',function(req,res){
+    res.render("pages/allTrucks")
+});
+
+router.get('/driverDetail',function(req,res){
+    res.render("pages/driverDetain")
+});
+
+router.get('/trucksDetail',function(req,res){
+    res.render("pages/trucksDetail")
+});
+
 
 
 
